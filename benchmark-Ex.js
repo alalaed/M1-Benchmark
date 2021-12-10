c57b371d5361d3df7f4b334a3c2b161e72ad96f2
Ali Alaeddine <a.alaeddine@live.com>
2021-12-10 13:55:58 +0100

    ex 30

--- benchmark-Ex.js
+++ benchmark-Ex.js
@@ -106,7 +106,7 @@ const arrayOfArrays = function(){
 console.log(arrayOfArrays(n))
 
 
-//ex29
+// ex 29
 
 let arr1 = [1,2,3,4,5,6]
 let arr2 = [1,2,3,4,5,6,7,8,9]
@@ -120,3 +120,16 @@ const compare = function(x,y){
 }
 
 compare(arr1,arr2)
+
+// ex 30 
+
+const sumOfArray = function(x,y){
+    let sum=0
+    for(i=0; i<x.length; i++){
+        sum += x[i]
+    }
+        for(i=0; i<y.length; i++){
+            sum += y[i]
+        }console.log(sum) 
+}
+sumOfArray(arr1,arr2)
--- trial.js
+++ trial.js
@@ -1,12 +1,13 @@
 let arr1 = [1,2,3,4,5,6]
 let arr2 = [1,2,3,4,5,6,7,8,9]
 
-const compare = function(x,y){
-    if (x.length>y.length){
-        console.log("The longer array is:" + x)
-    } else{
-        console.log("The longer array is:" + y)
+const sumOfArray = function(x,y){
+    let sum=0
+    for(i=0; i<x.length; i++){
+        sum += x[i]
     }
+        for(i=0; i<y.length; i++){
+            sum += y[i]
+        }console.log(sum) 
 }
-
-compare(arr1,arr2)
+sumOfArray(arr1,arr2)
