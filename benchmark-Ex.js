e1c375d5fbbd7ede1669ec14cfc3de946361fe57
Ali Alaeddine <a.alaeddine@live.com>
2021-12-10 11:17:58 +0100

    ex 21 22 23

--- benchmark-Ex.js
+++ benchmark-Ex.js
@@ -1,3 +1,13 @@
 let x = "John"
 let y = "Doe"
 console.log(x + " <> " + y)
+
+
+let student = {
+    name: "Ali",
+    surname: "Alaeddine",
+    email:"a.alaeddine@live.com"
+}
+
+delete student.email
+console.log(student)
